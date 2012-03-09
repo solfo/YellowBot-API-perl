8104dd543097abd8bdb5db8699e423177401c96f
Adriano Ferreira <ferreira@cpan.org>
2012-03-09 15:52:20 -0300

    Fix POD of YellowBot::API::Request::JSON

--- lib/YellowBot/API/Request/JSON.pm
+++ lib/YellowBot/API/Request/JSON.pm
@@ -58,7 +58,7 @@ __END__
 
 =head1 NAME
 
-YellowBot::API::Request::Json - Request object for YellowBot::API (JSON POST)
+YellowBot::API::Request::JSON - Request object for YellowBot::API (JSON POST)
 
 =head1 SYNOPSIS
 
@@ -67,7 +67,7 @@ This class manages setting up JSON POST requests for the YellowBot::API.
 No user servicable parts inside.  This part of the API is subject to change.
 
 
-    my $req = YellowBot::API::Request::Json->new
+    my $req = YellowBot::API::Request::JSON->new
        (api    => $yellowbot_api,
         method => 'location/detail',
         args   => { foo => 'bar',
